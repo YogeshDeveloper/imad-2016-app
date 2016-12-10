4622fe3f11ea88a8857438cdab5b7760c56387c6
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-10 12:29:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
