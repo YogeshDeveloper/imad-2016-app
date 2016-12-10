f327ba204d9efb2f0667922707bb3bf60a1215c1
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-10 12:27:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,59 +13,59 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('img/demo-screen-1.jpg', function (req, res) {
+app.get('/img/demo-screen-1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'img', 'demo-screen-1.jpg'));
 });
 
-app.get('img/bg-cta.jpg', function (req, res) {
+app.get('/img/bg-cta.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'img', 'bg-cta.jpg'));
   });
 
-app.get('img/bg-pattern.png', function (req, res) {
+app.get('/img/bg-pattern.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'img', 'bg-pattern.png'));
   });
   
-app.get('css/new-age.css', function (req, res) {
+app.get('/css/new-age.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'css', 'new-age.css'));
   });
 
-app.get('css/new-age.min.css', function (req, res) {
+app.get('/css/new-age.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'css', 'new-age.min.css'));
   });
   
-app.get('vendor/bootstrap/css/bootstrap.css', function (req, res) {
+app.get('/vendor/bootstrap/css/bootstrap.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/bootstrap/css', 'bootstrap.css'));
   });
   
-app.get('vendor/bootstrap/css/bootstrap.min.css', function (req, res) {
+app.get('/vendor/bootstrap/css/bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/bootstrap/css', 'bootstrap.min.css'));
   });
   
-app.get('vendor/device-mockups/device-mockups.css', function (req, res) {
+app.get('/vendor/device-mockups/device-mockups.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/device-mockups', 'device-mockups.css'));
   });
   
-app.get('vendor/device-mockups/device-mockups2.css', function (req, res) {
+app.get('/vendor/device-mockups/device-mockups2.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/device-mockups/device-mockups2.css', 'device-mockups2.css'));
   });
   
-app.get('vendor/device-mockups/device-mockups.min.css', function (req, res) {
+app.get('/vendor/device-mockups/device-mockups.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/device-mockups', 'device-mockups.min.css'));
   });
   
-app.get('vendor/device-mockups/device-mockups2.min.css', function (req, res) {
+app.get('/vendor/device-mockups/device-mockups2.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/device-mockups', 'device-mockups2.min.css'));
   });
   
-app.get('vendor/font-awesome/css/font-awesome.css', function (req, res) {
+app.get('/vendor/font-awesome/css/font-awesome.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/font-awesome/css', '/font-awesome.css'));
   });
   
-app.get('vendor/font-awesome/css/font-awesome.min.css', function (req, res) {
+app.get('/vendor/font-awesome/css/font-awesome.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/font-awesome/css', '/font-awesome.min.css'));
   });
   
-app.get('vendor/simple-line-icons/css/simple-line-icons.css', function (req, res) {
+app.get('/vendor/simple-line-icons/css/simple-line-icons.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/css', 'simple-line-icons.css'));
   });
   
