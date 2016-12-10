ef49a1e85836c161998842e838543e107b7f3df3
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-10 13:12:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-var font=require('font-awesome');
 var session = require('express-session');
 
 var express = require('express');
