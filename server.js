a20ca74bcaa05211e147fc73c1a23b12cdfe8320
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-10 23:49:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,9 +81,7 @@ app.get('/vendor/simple-line-icons/css/simple-line-icons.css', function (req, re
   res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/css', 'simple-line-icons.css'));
   });
   
-app.get('/less/mixins.less', function (req, res) {
-  res.sendFile(path.join(__dirname, 'less', 'mixins.less'));
-  });
+
   
 app.get('/ui/yogi.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'yogi.gif'));
@@ -92,46 +90,6 @@ app.get('/ui/yogi.gif', function (req, res) {
 app.get('/blog.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
   });
-
-app.get('/less/mixins.less', function (req, res) {
-  res.sendFile(path.join(__dirname, 'less', 'mixins.less'));
-  });
-  
-
-app.get('/less/variables.less', function (req, res) {
-  res.sendFile(path.join(__dirname, 'less', 'variables.less'));
-  });
-  
-app.get('vendor/simple-line-icons/fonts/Simple-Line-Icons.eot', function (req, res) {
-  res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/fonts', 'Simple-Line-Icons.eot'));
-  });
-  
-app.get('vendor/simple-line-icons/fonts/Simple-Line-Icons.svg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/fonts', 'Simple-Line-Icons.svg'));
-  });
-  
-app.get('vendor/simple-line-icons/fonts/Simple-Line-Icons.ttf', function (req, res) {
- res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/fonts', 'Simple-Line-Icons.ttf'));
- });
-  
-app.get('vendor/simple-line-icons/fonts/Simple-Line-Icons.woff', function (req, res) {
- res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/fonts', 'Simple-Line-Icons.woff'));
- });
-  
-app.get('vendor/simple-line-icons/fonts/Simple-Line-Icons.woff2', function (req, res) {
-  res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/fonts', 'Simple-Line-Icons.woff2'));
-  });
-  
-app.get('vendor/simple-line-icons/less/simple-line-icons.less', function (req, res) {
-  res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/less', 'simple-line-icons.less'));
-  });
-  
-app.get('vendor/simple-line-icons/scss/simple-line-icons.scss', function (req, res) {
-  res.sendFile(path.join(__dirname, 'vendor/simple-line-icons/scss', 'simple-line-icons.scss'));
-  });
-
-  
-  
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
