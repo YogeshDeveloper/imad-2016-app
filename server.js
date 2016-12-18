a652a6e290cedd7cf2e33f4bc08fc75ca347348a
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 18:09:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
+var pool = new Pool(config);
+app.get('/test-db',function (req, res){
+    //make a select request 
+    // return a response with the results
+    pool.query('SELECT * FROM test'),function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else {
+          res.send(JSON.stringify(result));
+      }
+  });
+});
 
 
   
