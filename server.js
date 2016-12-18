2bb353f00af718c6a7196840c6405680fe2ffd36
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 14:15:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var session = require('express-session');
 //console.log(process.env.DB_PASSWORD);
 
 var config = {
-    user: 'yogeshdeveloper',
-    database: 'yogeshdeveloper',
+    user: 'coco98',
+    database: 'coco98',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
@@ -20,64 +20,120 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
-    secret: 'spmeRandomSecretValue',
-    cookie:{ maxAgge: 1000 * 60 * 60 * 24 * 30}
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-app.get('/ui/Blog.html', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui', 'Blog.html'));
-});
-
-function hash (input,salt) {
-    var hashed =  crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
+                ${content}
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
 
-app.get('/hash/:input', function(req,res) {
-    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-    res.send(hashedString);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res) {
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
 app.post('/create-user', function (req, res) {
-    var username=req.body.username;
-     var password=req.body.password;
-    var salt=crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username, dbString],function (err,result){
-         if (err){
-            res.status(500).send(err.toString());
-        }else {
-            res.send('User successfully created: '+ username);
-        }
-        
-    });
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
+
 app.post('/login', function (req, res) {
-    var username=req.body.username;
-     var password=req.body.password;
+   var username = req.body.username;
+   var password = req.body.password;
    
-    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function (err,result){
-         if (err){
-            res.status(500).send(err.toString());
-        }else {
-            if (result.rows.length===0){
-                res.send(403).send('usename/password is invalid');
-            }else {
-                var dbString=result.rows[0].password;
-               var salt=dbString.split('$')[2];
-               var hashedPassword=hash(password,salt);
-               if(hashedPassword===dbString){
-                    req.session.auth = {userId: result.rows[0].id};
-               res.send('credentials correct!'); 
-               }else {
-                    res.send(403).send('usename/password is invalid');
-               
-               }
-            }
-           
-        }
-        
-    });
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
 });
 
 app.get('/check-login', function (req, res) {
@@ -173,24 +229,10 @@ app.get('/articles/:articleName', function (req, res) {
   });
 });
 
-var counter = 0;
-app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString());
-});
-var counter1 = 0;
-app.get('/counter1',function(req,res){
-    counter1 = counter1 + 1;
-    res.send(counter1.toString());
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
-var names = [];
-app.get('/submit-name', function(req,res){
-    var name = req.query.name;                                                                                      
-    names.push(name); 
-   res.send(JSON.stringify(names));
-});
-  
 
 var express = require('express');
 var morgan = require('morgan');
