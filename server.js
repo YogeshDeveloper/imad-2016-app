4db4c546a68f056535d89eed7d4c2717dfb991fd
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 14:10:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,188 @@ var session = require('express-session');
 
 //console.log(process.env.DB_PASSWORD);
 
+var config = {
+    user: 'yogeshdeveloper',
+    database: 'yogeshdeveloper',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'spmeRandomSecretValue',
+    cookie:{ maxAgge: 1000 * 60 * 60 * 24 * 30}
+}));
+
+app.get('/ui/Blog.html', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui', 'Blog.html'));
+});
+
+function hash (input,salt) {
+    var hashed =  crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+
+app.get('/hash/:input', function(req,res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+app.post('/create-user', function (req, res) {
+    var username=req.body.username;
+     var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username, dbString],function (err,result){
+         if (err){
+            res.status(500).send(err.toString());
+        }else {
+            res.send('User successfully created: '+ username);
+        }
+        
+    });
+});
+app.post('/login', function (req, res) {
+    var username=req.body.username;
+     var password=req.body.password;
+   
+    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function (err,result){
+         if (err){
+            res.status(500).send(err.toString());
+        }else {
+            if (result.rows.length===0){
+                res.send(403).send('usename/password is invalid');
+            }else {
+                var dbString=result.rows[0].password;
+               var salt=dbString.split('$')[2];
+               var hashedPassword=hash(password,salt);
+               if(hashedPassword===dbString){
+                    req.session.auth = {userId: result.rows[0].id};
+               res.send('credentials correct!'); 
+               }else {
+                    res.send(403).send('usename/password is invalid');
+               
+               }
+            }
+           
+        }
+        
+    });
+});
+
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+});
+
+var pool = new Pool(config);
+
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+app.post('/submit-comment/:articleName', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                } else {
+                    var articleId = result.rows[0].id;
+                    // Now insert the right comment for this article
+                    pool.query(
+                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        [req.body.comment, articleId, req.session.auth.userId],
+                        function (err, result) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                res.status(200).send('Comment inserted!')
+                            }
+                        });
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
+});
+
+app.get('/articles/:articleName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+});
+
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+var counter1 = 0;
+app.get('/counter1',function(req,res){
+    counter1 = counter1 + 1;
+    res.send(counter1.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req,res){
+    var name = req.query.name;                                                                                      
+    names.push(name); 
+   res.send(JSON.stringify(names));
+});
   
 
 var express = require('express');
