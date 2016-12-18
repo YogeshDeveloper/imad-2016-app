38b794d3a2cd49c4829c0e0b7993de17ba3d148c
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-19 01:41:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+function hash (input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
 app.get('/hash/:input', function(req, res) {
    var hashedString = hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
