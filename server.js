5e2980e9d156c402dd85614cb32e43773c04cc76
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 14:18:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/ui/Blog.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Blog.html'));
 });
 
 
