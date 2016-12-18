d3dd1cc3509ecb776253797d16b685feb6d7f75e
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 19:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -254,6 +254,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/article.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '/article.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
