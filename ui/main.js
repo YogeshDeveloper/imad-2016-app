0a94de803f65943e0c4e9d46e247024a50222f9a
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 10:43:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,21 @@
 function loadLoginForm () {
     var loginHtml = `
-        <h3>Login/Register to unlock awesome features</h3>
-        <input type="text" id="username" placeholder="username" />
-        <input type="password" id="password" />
+      
+       <h4 style="color:white">Username:</h4><input type="text" id="username" placeholder="username" /> <br/><br/>
+       <h4 style="color:white">Password:</h4><input type="password" id="password" />
+       
         <br/><br/>
-        <input type="submit" id="login_btn" value="Login" />
+       <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
         `;
+                
     document.getElementById('login_area').innerHTML = loginHtml;
+
     
     // Submit username/password to login
     var submit = document.getElementById('login_btn');
     submit.onclick = function () {
+        
         // Create a request object
         var request = new XMLHttpRequest();
         
@@ -21,6 +25,7 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   submit.value = 'Sucess!';
+                  window.open('http://nusrathyasin.imad.hasura-app.io/ui/homepage.html');
               } else if (request.status === 403) {
                   submit.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
@@ -58,7 +63,10 @@ function loadLoginForm () {
               // Take some action
               if (request.status === 200) {
                   alert('User created successfully');
-                  register.value = 'Registered!';
+                                
+                       register.value = 'Registered!';
+
+             
               } else {
                   alert('Could not register the user');
                   register.value = 'Register';
@@ -77,15 +85,24 @@ function loadLoginForm () {
         register.value = 'Registering...';
     
     };
+    
+
 }
 
+
+
+
 function loadLoggedInUser (username) {
-    var loginArea = document.getElementById('login_area');
+    
+    var loginArea = document.getElementById('login');
     loginArea.innerHTML = `
         <h3> Hi <i>${username}</i></h3>
         <a href="/logout">Logout</a>
     `;
+    
+    
 }
+
 
 function loadLogin () {
     // Check if the user is already logged in
@@ -93,7 +110,11 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
+                 
                 loadLoggedInUser(this.responseText);
+                
+                
+            
             } else {
                 loadLoginForm();
             }
@@ -104,6 +125,9 @@ function loadLogin () {
     request.send(null);
 }
 
+loadLogin();
+
+////////
 function loadArticles () {
         // Check if the user is already logged in
     var request = new XMLHttpRequest();
@@ -131,8 +155,87 @@ function loadArticles () {
 }
 
 
-// The first thing to do is to check if the user is logged in!
-loadLogin();
 
 // Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
+////
+//counter code
+function getCounter(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+};
+ request.open('GET', '/counter', true);
+    request.send(null);
+}
+
+getCounter();
+//
+var button = document.getElementById('counter1');
+//var counter=0;
+button.onclick = function() {
+   // counter = counter+1;
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status ===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count1');
+            span.innerHTML = counter.toString();
+            
+            
+
+        }
+    }
+    
+};
+//make the request
+request.open('GET','http://nusrathyasin.imad.hasura-app.io/counter1',true);
+request.send(null);
+};
+//
+var submit =document.getElementById('submit_btn');
+submit.onclick = function() {
+    //create a request object
+var request = new XMLHttpRequest();
+	request.onreadystatechange=function(){
+		if(request.readyState===XMLHttpRequest.DONE)
+		{
+			if(request.status===200)
+			{
+				var names= request.responseText;
+				
+				names = JSON.parse(names);
+				var list="";
+				for(var i=0;i<names.length;i++)
+				{
+				  
+				
+			                    list += '<li  class="commentbox">' + names[i] + '</li>';
+				}
+			var ul = document.getElementById('namelist');
+			list+="<p id='db'></p><br/>";
+			ul.innerHTML=list;
+			}
+		}
+	
+	};
+
+
+   var nameInput =document.getElementById('name');
+var name = nameInput.value;
+//var firstname = document.getElementById('firstname').value;
+
+request.open('GET','http://yogeshdeveloper.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(null);
+
+};
