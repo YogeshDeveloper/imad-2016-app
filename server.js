b5ab5d9ff31ef7afe3883b11384eb6ba8b35e07a
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-18 18:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ app.get('/test-db',function (req, res){
             res.status(500).send(err.toString());
         }else {
           res.send(JSON.stringify(result));
-      }
-  }
+        }
+    });
+      
 });
 
 
