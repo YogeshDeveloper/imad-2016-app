3d591ad9c118b46998302351cbd81c68bfd361cc
YogeshDeveloper <spyogesh1994@gmail.com>
2016-12-19 05:42:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,10 +21,13 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-//app.use(session({
-   // secret: 'someRandomSecretValue',
-  //  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
-//}));
+app.use(session({
+    secret: 'someRandomSecretValue',
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 30},
+  resave: true,
+    saveUninitialized: true
+
+}));
 
 function hash (input, salt) {
     // How do we create a hash?
